4d1ee2116b62727c1bec08b4dc7874d479b75d6f
vortesnail <1091331061@qq.com>
2021-03-11 19:00:23 +0800

    chore: add stylelint plugin and configuration

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+  rules: {
+    'plugin/declaration-block-no-ignored-properties': true,
+    'comment-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'function-name-case': 'lower',
+    'no-descending-specificity': null,
+    'no-invalid-double-slash-comments': null,
+    'rule-empty-line-before': 'always',
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+  ignoreFiles: ['node_modules/**/*', 'build/**/*', 'dist/**/*'],
+};
